f19d94b2d6293bd7a8fc96d0255ba041317d1025
Mark <markthehopeful@gmail.com>
2021-11-16 19:33:45 +0300

    Game class added with a function to conclude result

--- game.py
+++ game.py
@@ -1,2 +1,25 @@
+from typing import List, DefaultDict
+import enum
+from play_pair import PlayPair
+from collections import defaultdict
+
+
+class GameStatus(enum.Enum):
+    not_started = 0  # No results available
+    completed = 1  # Results are ready
+
+
 class Game:
-    pass  # FIXME: stub
+    def __init__(self, participants: List[PlayPair], words: List[str]):
+        self.participants: List[PlayPair] = participants
+        self.words: List[str] = words
+        self.state: GameStatus = GameStatus.not_started
+        self.result: DefaultDict[PlayPair, int] = defaultdict(int)
+
+    def conclude_result(self, results: DefaultDict[PlayPair, int]) -> bool:  # TODO: replace with an exception
+        if self.state == GameStatus.completed:
+            return False
+        if set(self.participants) != set(results.keys()):  # FIXME: bad way to do this
+            return False
+        self.result = results
+        return True
